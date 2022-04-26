52573de4c5ba7bc2786e8e1c6d50c81e77bd8f85
donnahuang <donnaphzj@gmail.com>
2022-04-25 22:58:26 -0400

    test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,18 @@
 const request = require('request');
 
-request.get('https://api.thecatapi.com/v1/breeds/search?q=sib', {
+const fetchBreedDescription = function(breedName, callback) {
+request.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, {
   headers: {
     'x-api-key': 'b01651ff-9bdc-4206-ae86-321fcd25db6d'
   }
 }, (err, res, body) => {
-  console.log(JSON.parse(body));
+ 
+  const obj = JSON.parse(body)[0];
+
+  const description = obj.description;
+
+  callback(err, description);
 })
+};
+
+module.exports = { fetchBreedDescription };
